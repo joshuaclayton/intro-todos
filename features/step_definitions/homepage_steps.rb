a990195e8202b0f040c0a975aa6c16fae4ccdcba
Joshua Clayton <jclayton@thoughtbot.com>
2012-03-26 14:02:11 -0400

    Refactoring

--- features/step_definitions/homepage_steps.rb
+++ features/step_definitions/homepage_steps.rb
@@ -13,7 +13,3 @@ end
 Then /^I should see the product description$/ do
   page.should have_css("[data-role='product-description']", text: /todo/)
 end
-
-Then /^I should be able to begin the registration process$/ do
-  page.should have_link("Sign up for an account", href: "/sign_up")
-end
--- features/step_definitions/session_steps.rb
+++ features/step_definitions/session_steps.rb
@@ -13,3 +13,7 @@ Then /^I should be signed out$/ do
   page.should_not have_css("[data-role='user-identification']")
   page.should_not have_link("Sign out")
 end
+
+Then /^I should be able to begin the registration process$/ do
+  page.should have_link("Sign up for an account", href: "/sign_up")
+end
