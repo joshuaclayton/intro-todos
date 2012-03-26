f9ec9067b20e7e559f70996e2c1602b01d8ef468
Joshua Clayton <jclayton@thoughtbot.com>
2012-03-26 13:46:58 -0400

    Allow users to sign out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,4 +6,9 @@ class SessionsController < ApplicationController
     session[:current_user] = params[:user][:email]
     redirect_to root_path
   end
+
+  def destroy
+    session[:current_user] = nil
+    redirect_to root_path
+  end
 end
---
+++ features/step_definitions/session_steps.rb
@@ -0,0 +1,15 @@
+Given /^I have logged in as "([^"]*)"$/ do |email_address|
+  steps %{
+    Given I am on the homepage
+    When I register for an account with the email "#{email_address}"
+  }
+end
+
+When /^I sign out$/ do
+  click_link "Sign out"
+end
+
+Then /^I should be signed out$/ do
+  page.should_not have_css("[data-role='user-identification']")
+  page.should_not have_link("Sign out")
+end
